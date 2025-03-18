f11a0559f4d07314b97d6c0493c67c481cca57ab
GuilhermeHMG <guilherme.ghg00@gmail.com>
2025-03-18 13:03:47 -0300

    commit v3.4

--- prisma/migrations/20250318160225_init/migration.sql
+++ prisma/migrations/20250318160225_init/migration.sql
@@ -15,3 +15,13 @@ CREATE TABLE `UserInfo` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Message` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `text` TEXT NOT NULL,
+    `sender` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
